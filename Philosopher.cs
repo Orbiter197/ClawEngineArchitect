4a0e780d6cd4eb10a75bef45936196514d888500
Sviatoslav Shmarin <seshmarin@edu.hse.ru>
2024-01-13 17:01:02 +0300

    refactor: add state to philosopher constructor

--- Philosopher.cs
+++ Philosopher.cs
@@ -5,10 +5,11 @@ namespace ClawEngineArchitect
     public class Philosopher
     {
         public float Coordinate {get; private set;}
-        public PhilosopherState State {get; private set; }
+        public PhilosopherState State {get; private set;}
 
         Philosopher(float coordinate) {
             Coordinate = coordinate;
+            State = PhilosopherState.Thinking;
         }
 
         public enum PhilosopherState {
